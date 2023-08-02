555a84d5412612218eabc5805c7456ab5b48809b
Denis Dankin <ulman33312@gmail.com>
2023-08-03 02:32:55 +0300

    fix:fixed toReadable function

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 module.exports = function toReadable(number) {
-    const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    const toTwenty = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    // Массивы словесных представлений чисел
+    const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+        'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-    let digitsArray = number.toString().split('');
-    switch (digitsArray.length) {
-        case 1: // 0 - 9
-            return units[digitsArray[0]];
-        case 2:
-            if (+digitsArray[0] === 1) { // 10 - 19
-                return toTwenty[digitsArray[1]];
-            } else { // 20 - 99
-                let result = tens[digitsArray[0] - 2];
-                return +digitsArray[1] === 0 ? result : `${result} ${units[digitsArray[1]]}`;
-            }
-        case 3: // 100 - 999
-            let remainder = number - 100 * digitsArray[0];
-            let result = `${units[digitsArray[0]]} hundred`;
-            return remainder === 0 ? result : `${result} ${toReadable(remainder)}`;
+    if (number === 0) {
+        return 'zero';
+    } else if (number < 20) {
+        return units[number];
+    } else if (number < 100) {
+        return tens[Math.floor(number / 10)] + (number % 10 !== 0 ? ' ' + units[number % 10] : '');
+    } else {
+        return units[Math.floor(number / 100)] + ' hundred' + (number % 100 !== 0 ? ' ' + toReadable(number % 100) : '');
     }
 }
